66df239cdf8d0d3c3869ca18cec6a96b202f70b6
lcisterna <lukas.cis91@gmail.com>
2020-04-23 12:43:08 -0300

    Update Hello.cpp

--- 00-CppHelloWorld/Hello.cpp
+++ 00-CppHelloWorld/Hello.cpp
@@ -1,1 +1,12 @@
+/*Programa Hello World!
+Objetivo: Imprimir en pantalla el mensaje "Hello World!"
+Autor: Cisterna Lucas
+Ultima modificación: 20200423 */
 
+#include <iostream>
+ 
+int main () 
+{
+    std::cout << "Hola, mundo";
+    return 0;
+}
