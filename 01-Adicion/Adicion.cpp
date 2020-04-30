e292c60f78af947638bfff555c80815490c6847b
lcisterna <lukas.cis91@gmail.com>
2020-04-30 00:33:11 -0300

    Create Adicion.cpp

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,21 @@
+/* Programa de Adicción TP 01
+   Lucas Adolfo Cisterna
+   29042020
+*/
+
+#include <iostream>
+
+int main () {
+
+    int num1, num2, SUMA=0 ; 
+    
+    std::cout << "Ingrese un numero entero,\n" ; 
+    std::cin >> num1;    
+    std::cout << "Ingrese otro numero entero,\n" ;
+    std::cin >> num2;
+    
+    SUMA = num1 + num2
+    
+    std::cout << "La suma de los numeros ingresados es",<<SUMA<<;        
+
+}
